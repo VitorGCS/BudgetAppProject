d546425a6901536f96d0f493228fec98b76ea9f6
Vitor Gil <vgc_silva@hotmail.com>
2018-09-26 15:39:35 +0100

    Project Planning and Architecture.
    Implementing the Module Pattern MVC

---
+++ starter/app.js
@@ -0,0 +1,35 @@
+//Architecture MVC
+
+var budgetController = (function(){
+    
+    var x = 23;
+    var add = function(a){
+        return x + a;
+    }
+    
+    return {
+        publicTest: function(b){
+            return add(b);
+        }
+    }
+    
+})();
+
+
+
+var UIController = (function(){
+    //Some Code
+    
+})();
+
+
+
+var controller = (function(budgetCtrl, UICtrl){
+    //Some Code
+    var z = budgetCtrl.publicTest(5);
+    return {
+        anotherPublic: function(){
+            console.log(z);
+        }
+    }
+})(budgetController, UIController);
