f3ae3f77d193836e658c02437eeb0bbaf77150ea
Vitor Gil <vgc_silva@hotmail.com>
2018-10-13 13:22:24 +0100

    Creating Income and Expense Function Constructors

--- starter/app.js
+++ starter/app.js
@@ -1,8 +1,29 @@
 //Architecture MVC
 
-//BEDGET CONTROLLER
+//BEDGET CONTROLLER - MODEL
 var budgetController = (function(){
-
+    var Expense = function(id, description, value){
+        this.id=id;
+        this.description = description;
+        this.value = value;
+    };
+    
+    var Income = function(id, description, value){
+        this.id=id;
+        this.description = description;
+        this.value = value;
+    };
+    
+    var data = {
+        allItems : {
+            exp: [],
+            inc: []
+        },
+        totals : {
+            exp : 0,
+            inc : 0
+        }
+    };
 
 })();
 
